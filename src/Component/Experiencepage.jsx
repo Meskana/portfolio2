2d24cb2012644f9d16842783143fa6a005fea4d4
Meskana <marcelinusilonuba21@gmail.com>
2024-10-16 08:50:18 +0100

    add project section

--- src/Component/Experiencepage.jsx
+++ src/Component/Experiencepage.jsx
@@ -1,93 +1,60 @@
+import image from "../images/coin.png";
+import image2 from "../images/hotel.png";
+import image3 from "../images/temp.png";
 const Experience = () => {
   return (
     <div className="container-exp">
-        <div className="container-bnt">
-      <button className="exp-bnt">Experince</button>
+      <div className="container-bnt">
+        <button className="exp-bnt">Projects</button>
       </div>
-      <div className="exp-con">
-        <h1>My Work Experince</h1>
-        <div className="exp-cont">
-          <div className="year">
-            <h4>2018_2019</h4>
-            <div className="exp">
-              <div className="exp-content">
-                <h4>Unizik</h4>
-                <h6>Web Desiger & Development</h6>
-                <p>
-                  In summary, as a web designer and writer, I bring a unique
-                  blend of creativity, technical expertise, and strong
-                  communication skills to every project I undertake. Whether
-                </p>
-              </div>
-            </div>
-          </div>
 
-          <div className="year">
-            <h4>2019_2020</h4>
-            <div className="exp">
-              <div className="exp-content">
-                <h4>Federal Collage</h4>
-                <h6>Graphic Desiger & Photograph</h6>
-                <p>
-                  In summary, as a web designer and writer, I bring a unique
-                  blend of creativity, technical expertise, and strong
-                  communication skills to every project I undertake. Whether
-                </p>
-              </div>
-            </div>
+      <section class="section3">
+        <h1>My Personal/Fuctuerd Project</h1>
+        <div className="sec3">
+          <div className="box">
+            <img src={image} className="project-image" alt="passport" />
+            <p>
+              took an initiative of opening the company to fill the gap. it aims
+              at understanding its clients wishes, and marking them a reality'
+            </p>
+            <button className="btnt">read more</button>
           </div>
-
-          <div className="year">
-            <div className="exp-year">
-              <h4>2020_2021</h4>
-            </div>
-            <div className="exp">
-              <div className="exp-content">
-                <h4>Binance Nigeria</h4>
-                <h6>User Interface</h6>
-                <p>
-                  In summary, as a web designer and writer, I bring a unique
-                  blend of creativity, technical expertise, and strong
-                  communication skills to every project I undertake. Whether
-                </p>
-              </div>
-            </div>
+          <div className="box">
+            <img src={image2} className="project-image" alt="passport" />
+            <p>
+              took an initiative of opening the company to fill the gap. it aims
+              at understanding its clients wishes, and marking them a reality'
+            </p>
+            <button className="btnt">read more</button>
           </div>
-
-          <div className="year">
-            <h4>2021_2022</h4>
-            <div className="exp">
-              <div className="exp-content">
-                <h4>Upwork</h4>
-                <h6>Brand Design</h6>
-                <p>
-                  In summary, as a web designer and writer, I bring a unique
-                  blend of creativity, technical expertise, and strong
-                  communication skills to every project I undertake. Whether
-                </p>
-              </div>
-            </div>
+          <div className="box">
+            <img src={image3} className="project-image" alt="passport" />
+            <p>
+              took an initiative of opening the company to fill the gap. it aims
+              at understanding its clients wishes, and marking them a reality'
+            </p>
+            <button className="btnt">read more</button>
           </div>
         </div>
-      </div>
+      </section>
       <div className="exp-box">
         <div>
-          <h1>2500+</h1>
+          <h1>2+</h1>
           <p> Project Complet</p>
         </div>
 
         <div>
-          <h1>1200+</h1>
+          <h1>1+</h1>
           <p> Win Award</p>
         </div>
 
         <div>
-          <h1>1000+</h1>
+          <h1>0+</h1>
           <p> Complet Cliant</p>
         </div>
 
         <div>
-          <h1>10+</h1>
+          <h1>0+</h1>
           <p> Years Experince</p>
         </div>
       </div>
